2e9f30ce5f7dc0ca7731307693b3c8f674453228
Tam Hoa <letamhoa2009@gmail.com>
2023-09-21 21:29:35 +0700

    Update HTML,CSS

---
+++ Doan/Doan/Controllers/IndexController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Doan.Controllers
+{
+    public class IndexController : Controller
+    {
+        // GET: Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            return View();
+        }
+        public ActionResult Coffee()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+    }
+}
